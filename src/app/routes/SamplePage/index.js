efd6d6989993f3166bd90efce3847271a00fd65a
Roman Bednyakov <rbednyakov@mentalstack.com>
2018-11-13 15:17:52 +0300

    first commit

---
+++ src/app/routes/SamplePage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import ContainerHeader from 'components/ContainerHeader/index';
+// import IntlMessages from 'util/IntlMessages';
+
+class SamplePage extends React.Component {
+    render() {
+        return (
+            <div className="app-wrapper">
+              <div className="app-calendar animated slideInUpTiny animation-duration-3">
+                <div>111</div>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default SamplePage;
